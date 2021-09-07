39cfad82bc6abafe9df40cfd10e0b306fb6d6034
Rahan <wangleibniz01@gmail.com>
2021-09-07 22:34:48 +0800

    first commit

---
+++ data/navigation.js
@@ -0,0 +1,50 @@
+export const navigationList = [
+  {
+    icon: "./feed.png",
+    iconActive: "./feedActive.png",
+    name: "Feed",
+    route: "/",
+  },
+
+  {
+    icon: "./freinds.png",
+    iconActive: "",
+    name: "Friends",
+    route: "/friends",
+  },
+
+  {
+    icon: "./event.png",
+    iconActive: "",
+    name: "Event",
+    route: "/events",
+  },
+
+  {
+    icon: "./videos.png",
+    iconActive: "",
+    name: "Watch Videos",
+    route: "/videos",
+  },
+
+  {
+    icon: "./photos.png",
+    iconActive: "",
+    name: "Photos",
+    route: "/photos",
+  },
+
+  {
+    icon: "./files.png",
+    iconActive: "",
+    name: "Files",
+    route: "/files",
+  },
+
+  {
+    icon: "./marketplace.png",
+    iconActive: "",
+    name: "MarketPlace",
+    route: "/marketplace",
+  },
+]
---
+++ data/stories.js
@@ -0,0 +1,26 @@
+export const storiesList = [
+  {
+    id: "1",
+    userImage: "",
+    username: "Rahan Bouess",
+    storyImages: [],
+  },
+  {
+    id: "2",
+    userImage: "",
+    username: "Kati Waters",
+    storyImages: [],
+  },
+  {
+    id: "3",
+    userImage: "",
+    username: "Denis francis ",
+    storyImages: [],
+  },
+  {
+    id: "4",
+    userImage: "",
+    username: "Bradley rivera",
+    storyImages: [],
+  },
+]
